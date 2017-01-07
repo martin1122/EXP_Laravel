a173cbf2b06942994066328d178eb495446acd79
Martin <martin1122@livemail.tw>
2017-01-07 14:49:25 +0800

    update newsSeeds.php

--- database/seeds/newsSeeds.php
+++ database/seeds/newsSeeds.php
@@ -15,6 +15,7 @@ class newsSeeds extends Seeder
     	foreach (range(1,1000) as $index) {
 	        DB::table('main_news')->insert([
 	            'title'        => $faker->text($maxNbChars = 50),
+                'catID'        => $faker->numberBetween($min = 1, $max = 5),
 	            'hasCover'     => $faker->numberBetween($min = 0, $max = 1),
                 'hasHead'      => $faker->numberBetween($min = 0, $max = 1) ,
                 'content'      => $faker->paragraph($nbSentences = 3, $variableNbSentences = true),
